a763effd95072aa81a7fcfd32ca6b587ca6c641f
rezaulislam54 <rezaulislam5490@gmail.com>
2024-08-23 12:02:01 +0600

    project complete

--- index.js
+++ index.js
@@ -60,7 +60,6 @@ const loadingHandelerSpiner = (isloading) => {
     }
 }
 
-
 // search field and search button cunnection
 const handelerSearch = (isShowAll) => {
     loadingHandelerSpiner(true);
@@ -76,7 +75,28 @@ const handleShowDetail = async (id) => {
     const data = await revs.json();
     const phoneData = data.data;
     console.log(phoneData);
+    showPhoneDetails(phoneData);
 }
+
+// phone Details Display
+ const showPhoneDetails = (phoneData) => {
+    const sowDetailContaine = document.getElementById('show-details-container');
+    sowDetailContaine.innerHTML = `
+    <img class="mx-auto p-5" src="${phoneData.image}" alt="">
+      <h1 class="font-bold my-3 text-2xl">${phoneData.name} </h1>
+      <p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+      <h1 class="text-sm mt-3"> <span class="text-lg font-medium">Brand : </span>${phoneData.brand}</h1>  
+      <h1 class="text-sm"> <span class="text-lg font-medium">Storage : </span>${phoneData.mainFeatures.storage}</h1>  
+      <h2 class="text-sm"> <span class="text-lg font-medium">ChipSet : </span>${phoneData.mainFeatures.chipSet}</h2>  
+      <h3 class="text-sm"> <span class="text-lg font-medium">Display Size : </span>${phoneData.mainFeatures.displaySize}</h3>  
+      <h3 class="text-sm"> <span class="text-lg font-medium">GPS : </span>${phoneData.others.GPS}</h3>        
+      <h2 class="text-sm"> <span class="text-lg font-medium">Release Date : </span>${phoneData.releaseDate}</h2>  
+    `;
+
+
+
+     my_modal_3.showModal();
+ }
 
 
 const handleShowAll = () =>{
